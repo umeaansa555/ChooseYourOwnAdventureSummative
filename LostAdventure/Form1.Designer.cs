070a1377586bb002ef0383fbde3720bb12383a52
P4 <umeaansa555@ed.amdsb.ca>
2021-10-27 13:34:35 -0400

    Add project files.

---
+++ LostAdventure/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿
+namespace LostAdventure
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.option2Label = new System.Windows.Forms.Label();
+            this.option1Label = new System.Windows.Forms.Label();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.output2Label = new System.Windows.Forms.Label();
+            this.option3Label = new System.Windows.Forms.Label();
+            this.option3Button = new System.Windows.Forms.PictureBox();
+            this.imageBox = new System.Windows.Forms.PictureBox();
+            this.option2Button = new System.Windows.Forms.PictureBox();
+            this.option1Button = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.option3Button)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.option2Button)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.option1Button)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // option2Label
+            // 
+            this.option2Label.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option2Label.ForeColor = System.Drawing.Color.White;
+            this.option2Label.Location = new System.Drawing.Point(67, 360);
+            this.option2Label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.option2Label.Name = "option2Label";
+            this.option2Label.Size = new System.Drawing.Size(225, 23);
+            this.option2Label.TabIndex = 10;
+            this.option2Label.Text = "option2Label";
+            // 
+            // option1Label
+            // 
+            this.option1Label.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option1Label.ForeColor = System.Drawing.Color.White;
+            this.option1Label.Location = new System.Drawing.Point(67, 324);
+            this.option1Label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.option1Label.Name = "option1Label";
+            this.option1Label.Size = new System.Drawing.Size(225, 23);
+            this.option1Label.TabIndex = 9;
+            this.option1Label.Text = "option1Label";
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BackColor = System.Drawing.Color.Transparent;
+            this.outputLabel.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.ForeColor = System.Drawing.Color.White;
+            this.outputLabel.Location = new System.Drawing.Point(24, 18);
+            this.outputLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(537, 82);
+            this.outputLabel.TabIndex = 8;
+            this.outputLabel.Text = "outputLabel";
+            // 
+            // output2Label
+            // 
+            this.output2Label.BackColor = System.Drawing.Color.Transparent;
+            this.output2Label.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.output2Label.ForeColor = System.Drawing.Color.White;
+            this.output2Label.Location = new System.Drawing.Point(24, 124);
+            this.output2Label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.output2Label.Name = "output2Label";
+            this.output2Label.Size = new System.Drawing.Size(537, 82);
+            this.output2Label.TabIndex = 12;
+            this.output2Label.Text = "output2Label";
+            // 
+            // option3Label
+            // 
+            this.option3Label.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option3Label.ForeColor = System.Drawing.Color.White;
+            this.option3Label.Location = new System.Drawing.Point(336, 324);
+            this.option3Label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.option3Label.Name = "option3Label";
+            this.option3Label.Size = new System.Drawing.Size(225, 23);
+            this.option3Label.TabIndex = 14;
+            this.option3Label.Text = "option3Label";
+            this.option3Label.Visible = false;
+            // 
+            // option3Button
+            // 
+            this.option3Button.Image = global::LostAdventure.Properties.Resources.arrowRight;
+            this.option3Button.Location = new System.Drawing.Point(293, 314);
+            this.option3Button.Margin = new System.Windows.Forms.Padding(2);
+            this.option3Button.Name = "option3Button";
+            this.option3Button.Size = new System.Drawing.Size(39, 32);
+            this.option3Button.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.option3Button.TabIndex = 13;
+            this.option3Button.TabStop = false;
+            this.option3Button.Visible = false;
+            this.option3Button.Click += new System.EventHandler(this.option3Button_Click);
+            // 
+            // imageBox
+            // 
+            this.imageBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.imageBox.Image = global::LostAdventure.Properties.Resources.deepBrush;
+            this.imageBox.Location = new System.Drawing.Point(24, 110);
+            this.imageBox.Margin = new System.Windows.Forms.Padding(2);
+            this.imageBox.Name = "imageBox";
+            this.imageBox.Size = new System.Drawing.Size(268, 174);
+            this.imageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imageBox.TabIndex = 11;
+            this.imageBox.TabStop = false;
+            // 
+            // option2Button
+            // 
+            this.option2Button.Image = global::LostAdventure.Properties.Resources.arrowRight;
+            this.option2Button.Location = new System.Drawing.Point(24, 350);
+            this.option2Button.Margin = new System.Windows.Forms.Padding(2);
+            this.option2Button.Name = "option2Button";
+            this.option2Button.Size = new System.Drawing.Size(39, 32);
+            this.option2Button.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.option2Button.TabIndex = 7;
+            this.option2Button.TabStop = false;
+            this.option2Button.Click += new System.EventHandler(this.option2Button_Click);
+            // 
+            // option1Button
+            // 
+            this.option1Button.Image = global::LostAdventure.Properties.Resources.arrowRight;
+            this.option1Button.Location = new System.Drawing.Point(24, 314);
+            this.option1Button.Margin = new System.Windows.Forms.Padding(2);
+            this.option1Button.Name = "option1Button";
+            this.option1Button.Size = new System.Drawing.Size(39, 32);
+            this.option1Button.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.option1Button.TabIndex = 6;
+            this.option1Button.TabStop = false;
+            this.option1Button.Click += new System.EventHandler(this.option1Button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(592, 422);
+            this.Controls.Add(this.option3Label);
+            this.Controls.Add(this.option3Button);
+            this.Controls.Add(this.output2Label);
+            this.Controls.Add(this.imageBox);
+            this.Controls.Add(this.option2Label);
+            this.Controls.Add(this.option1Label);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.option2Button);
+            this.Controls.Add(this.option1Button);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Choose Your Own Adventure!";
+            ((System.ComponentModel.ISupportInitialize)(this.option3Button)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.option2Button)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.option1Button)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox imageBox;
+        private System.Windows.Forms.Label option2Label;
+        private System.Windows.Forms.Label option1Label;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.PictureBox option2Button;
+        private System.Windows.Forms.PictureBox option1Button;
+        private System.Windows.Forms.Label output2Label;
+        private System.Windows.Forms.Label option3Label;
+        private System.Windows.Forms.PictureBox option3Button;
+    }
+}
+
---
+++ LostAdventure/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace LostAdventure
+{
+    public partial class Form1 : Form
+    {
+        // Tracks what page of the story the user is at
+        int page = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Display initial message and options
+            outputLabel.Text = "It's time you become a trainer. Your mom kicks you out";
+            output2Label.Text = "Where will you go?";
+            option1Label.Text = "Straight into the grass. No pokeballs. (55 % success)";
+            option2Label.Text = "Oak's house";
+
+            SoundPlayer junglePlayer = new SoundPlayer(Properties.Resources.jungle);
+            junglePlayer.Play();
+        }
+
+        private void option1Button_Click(object sender, EventArgs e)
+        {
+            /// Check what page we are currently on, and then flip
+            /// to the page you need to go to if you selected option 1
+
+            if (page == 1) { page = 2; }
+            else if (page == 2) { page = 99; }
+            else if (page == 3) { page = 5; }
+            else if (page == 4) { page = 6; }
+            else if (page == 5) { page = 6; }
+            else if (page == 6) { page = 98; }
+            else if (page == 7) { page = 9; }
+            else if (page == 9) { page = 96; }
+
+            else if (page == 99) { page = 1; }
+            else if (page == 98) { page = 1; }
+            else if (page == 97) { page = 1; }
+
+
+            /// Display text and game options to screen based on the 
+            /// current page
+
+            DisplayPage();
+        }
+
+        private void option2Button_Click(object sender, EventArgs e)
+        {
+            ///check what page we are currently on, and then flip
+            ///to the page you need to go to if you selected option 2
+
+            if (page == 1)
+            {
+                page = 3;
+            }
+            else if (page == 2) { page = 99; }
+            else if (page == 3) { page = 5; }
+            else if (page == 4) { page = 5; }
+            else if (page == 5) { page = 7; }
+            else if (page == 6) { page = 99; }
+            else if (page == 7) { page = 99; }
+            else if (page == 8) { page = 98; }
+            else if (page == 9) { page = 96; }
+
+            else if (page == 99) { page = 96; }
+            else if (page == 98) { page = 96; }
+            else if (page == 97) { page = 96; }
+
+
+            /// Display text and game options to screen based on the 
+            /// current page
+            /// 
+            DisplayPage();
+
+        }
+
+
+        private void option3Button_Click(object sender, EventArgs e)
+        {
+            ///check what page we are currently on, and then flip
+            ///to the page you need to go to if you selected option 2
+
+            if (page == 3) {page = 5; }
+            else if (page == 5) { page = 7; }
+
+
+            /// Display text and game options to screen based on the 
+            /// current page
+            /// 
+            DisplayPage();
+        }
+
+        private void DisplayPage()
+        {
+            switch (page)
+            {
+                case 1:
+                    // Display initial message and options
+                    outputLabel.Text = "It's time you become a trainer. Your mom kicks you out";
+                    output2Label.Text = "Where will you go?";
+                    option1Label.Text = "Straight into the grass. No pokeballs. (55 % success)";
+                    option2Label.Text = "Oak's house";
+                    SoundPlayer junglePlayer = new SoundPlayer(Properties.Resources.jungle);
+                    junglePlayer.Play();
+
+                    imageBox.Image = (Properties.Resources.deepBrush);
+                    break;
+                case 2:
+                    // Display page message and options
+                    outputLabel.Text = "You get pecked to death by a pidgey. Idiot";
+
+                    option1Label.Text = "Yes";
+                    option2Label.Text = ".";
+                    SoundPlayer brookPlayer = new SoundPlayer(Properties.Resources.brook);
+                    brookPlayer.Play();
+
+
+                    break;
+                case 3:
+                    // Display page message and options
+                    outputLabel.Text = "He offers you a starter pokemon";
+                    output2Label.Text = "Which do you pick?";
+                    option1Label.Text = "Charmander";
+                    option2Label.Text = "Squirtle";
+                    option3Label.Text = "Bulbasaur";
+                    option3Label.Visible = true;
+                    option3Button.Visible = true;
+
+                    break;
+                case 4:
+                    // Display page message and options
+                    outputLabel.Text = "Beating the odds, you made it through to Viridian City!";
+                    output2Label.Text = "What will you do in Viridian City?";
+                    option1Label.Text = "Talk to the local elderly men";
+                    option2Label.Text = "Challenge the gym (diifcult)";
+                    option3Label.Text = "Move on to the next city (which is a better fit for your level)";
+                    option3Label.Visible = true;
+                    option3Button.Visible = true;
+                    break;
+                case 5:
+                    // Display page message and options
+                    outputLabel.Text = $"Your pokemon gets you through the route safely. What will you do in Viridian City?";
+                    output2Label.Text = "What will you do in Viridian City?";
+                    option1Label.Text = "Talk to the local elderly men";
+                    option2Label.Text = "Challenge the gym (diifcult)";
+                    option3Label.Text = "Move on to the next city (which is a better fit for your level)";
+                    option3Label.Visible = true;
+                    option3Button.Visible = true;
+                    break;
+                case 6:
+                    // Display page message and options
+                    outputLabel.Text = $"He tells you suspiciously that he knows a place in Celadon where you can get rich (quick!), and that he's going there soon and can take you.";
+                    output2Label.Text = "Do you go to Celadon City with him? (OLD MAN)";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 7:
+                    // Display page message and options
+                    outputLabel.Text = $"It's much higher than your current skill level. The gym leader is also a gang boss. Do you still challenge Giovanni?";
+                    output2Label.Text = $"Do you still want to challenge the gym?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 8:
+                    outputLabel.Text = $"You get rich as promised and completely forget about your old life. You can't remember why you ever wanted to be a pokemon trainer at all. Play again?";
+                    output2Label.Text = $"OLD MAN ENDING";
+                    option1Label.Text = ".";
+                    option2Label.Text = ".";
+                    break;
+                case 9:
+                    outputLabel.Text = $"Your sad level 9 team gets swept. Try again from the beginning.";
+                    output2Label.Text = $"Play again??";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 99:
+                    // Display page message and options
+                    outputLabel.Text = "You black out and wake up at a pokemon center. Get up and try your journey again?";
+                    output2Label.Text = "Play again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+
+                case 98:
+                    outputLabel.Text = "You Win! Try to find the other endings ;)";
+                    output2Label.Text = "Play again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 97:
+                    outputLabel.Text = "You.. Win? Theres another win option! Try to find the other endings;)";
+                    output2Label.Text = "Play again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 96:
+                    outputLabel.Text = "Thanks for playing :)";
+                    output2Label.Text = " ";
+                    option1Label.Text = " ";
+                    option2Label.Text = " ";
+                    Thread.Sleep(3000);
+                    Application.Exit();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
