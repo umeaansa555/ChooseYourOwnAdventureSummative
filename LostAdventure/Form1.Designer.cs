97e6d8d2b1c60180214521deb8da45b4d2fe7050
P4 <umeaansa555@ed.amdsb.ca>
2021-10-28 13:15:30 -0400

    majority of playtesting done, adjusted rand chance

--- LostAdventure/Form1.Designer.cs
+++ LostAdventure/Form1.Designer.cs
@@ -53,7 +53,7 @@ namespace LostAdventure
             this.option2Label.Location = new System.Drawing.Point(67, 444);
             this.option2Label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.option2Label.Name = "option2Label";
-            this.option2Label.Size = new System.Drawing.Size(412, 23);
+            this.option2Label.Size = new System.Drawing.Size(434, 23);
             this.option2Label.TabIndex = 10;
             this.option2Label.Text = "option2Label";
             // 
@@ -65,7 +65,7 @@ namespace LostAdventure
             this.option1Label.Location = new System.Drawing.Point(67, 408);
             this.option1Label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.option1Label.Name = "option1Label";
-            this.option1Label.Size = new System.Drawing.Size(412, 23);
+            this.option1Label.Size = new System.Drawing.Size(434, 23);
             this.option1Label.TabIndex = 9;
             this.option1Label.Text = "option1Label";
             // 
@@ -73,14 +73,13 @@ namespace LostAdventure
             // 
             this.outputLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.outputLabel.AutoSize = true;
             this.outputLabel.BackColor = System.Drawing.Color.Transparent;
             this.outputLabel.Font = new System.Drawing.Font("OCR A Extended", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.outputLabel.ForeColor = System.Drawing.Color.Black;
             this.outputLabel.Location = new System.Drawing.Point(24, 18);
             this.outputLabel.Margin = new System.Windows.Forms.Padding(2);
             this.outputLabel.Name = "outputLabel";
-            this.outputLabel.Size = new System.Drawing.Size(95, 13);
+            this.outputLabel.Size = new System.Drawing.Size(477, 52);
             this.outputLabel.TabIndex = 8;
             this.outputLabel.Text = "outputLabel";
             // 
@@ -89,10 +88,10 @@ namespace LostAdventure
             this.output2Label.BackColor = System.Drawing.Color.Transparent;
             this.output2Label.Font = new System.Drawing.Font("OCR A Extended", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.output2Label.ForeColor = System.Drawing.Color.Black;
-            this.output2Label.Location = new System.Drawing.Point(24, 69);
+            this.output2Label.Location = new System.Drawing.Point(24, 72);
             this.output2Label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.output2Label.Name = "output2Label";
-            this.output2Label.Size = new System.Drawing.Size(537, 31);
+            this.output2Label.Size = new System.Drawing.Size(537, 21);
             this.output2Label.TabIndex = 12;
             this.output2Label.Text = "output2Label";
             // 
@@ -104,7 +103,7 @@ namespace LostAdventure
             this.option3Label.Location = new System.Drawing.Point(67, 480);
             this.option3Label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.option3Label.Name = "option3Label";
-            this.option3Label.Size = new System.Drawing.Size(412, 23);
+            this.option3Label.Size = new System.Drawing.Size(434, 23);
             this.option3Label.TabIndex = 14;
             this.option3Label.Text = "option3Label";
             this.option3Label.Visible = false;
@@ -127,10 +126,11 @@ namespace LostAdventure
             // 
             this.imageBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.imageBox.Image = global::LostAdventure.Properties.Resources.Pallet_Town_RBY;
-            this.imageBox.Location = new System.Drawing.Point(24, 102);
+            this.imageBox.Location = new System.Drawing.Point(24, 92);
             this.imageBox.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.imageBox.Name = "imageBox";
-            this.imageBox.Size = new System.Drawing.Size(477, 288);
+            this.imageBox.Size = new System.Drawing.Size(477, 304);
+            this.imageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.imageBox.TabIndex = 11;
             this.imageBox.TabStop = false;
             // 
@@ -165,7 +165,7 @@ namespace LostAdventure
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(522, 513);
+            this.ClientSize = new System.Drawing.Size(545, 516);
             this.Controls.Add(this.option3Label);
             this.Controls.Add(this.option3Button);
             this.Controls.Add(this.output2Label);
@@ -184,7 +184,6 @@ namespace LostAdventure
             ((System.ComponentModel.ISupportInitialize)(this.option2Button)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.option1Button)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
--- LostAdventure/Form1.cs
+++ LostAdventure/Form1.cs
@@ -24,7 +24,7 @@ namespace LostAdventure
             // Display initial message and options
             outputLabel.Text = "It's time you become a trainer. Your mom kicks you out";
             output2Label.Text = "Where will you go?";
-            option1Label.Text = "Straight into the grass. No pokeballs. (55 % success)";
+            option1Label.Text = "Straight into the grass. No pokeballs. (45 % success)";
             option2Label.Text = "Oak's house";
             option3Label.Visible = false;
             option3Button.Visible = false;
@@ -43,7 +43,7 @@ namespace LostAdventure
             {
                 Random randGen = new Random();
                 int randValue = randGen.Next(1, 101);
-                if (randValue < 55)
+                if (randValue < 45)
                 {
                     page = 2;
                 }
@@ -58,9 +58,23 @@ namespace LostAdventure
             else if (page == 3) { page = 5; }
             else if (page == 4) { page = 6; }
             else if (page == 5) { page = 6; }
-            else if (page == 6) { page = 98; }
+            else if (page == 6) { page = 8; }
             else if (page == 7) { page = 9; }
-            else if (page == 9) { page = 96; }
+            else if (page == 8) { page = 98; }
+            else if (page == 9) { page = 99; }
+
+            else if (page == 10) { page = 11; }
+            else if (page == 11) { page = 12; }
+            else if (page == 12) { page = 97; }
+            else if (page == 13) { page = 14; }
+            else if (page == 14) { page = 99; }
+            else if (page == 15) { page = 16; }
+            else if (page == 16) { page = 17; }
+            else if (page == 17) { page = 97; }
+            else if (page == 18) { page = 98; }
+
+
+
 
             else if (page == 99) { page = 1; }
             else if (page == 98) { page = 1; }
@@ -78,18 +92,39 @@ namespace LostAdventure
             ///check what page we are currently on, and then flip
             ///to the page you need to go to if you selected option 2
 
-            if (page == 1)
-            {
-                page = 3;
-            }
+            if (page == 1) { page = 3; }
             else if (page == 2) { page = 99; }
             else if (page == 3) { page = 5; }
             else if (page == 4) { page = 5; }
             else if (page == 5) { page = 7; }
-            else if (page == 6) { page = 99; }
-            else if (page == 7) { page = 99; }
+            else if (page == 6) { page = 5; }
+            else if (page == 7) { page = 5; }
             else if (page == 8) { page = 98; }
-            else if (page == 9) { page = 96; }
+            else if (page == 9) { page = 99; }
+
+            else if (page == 10) { page = 13; }
+            else if (page == 12) { page = 97; }
+
+            else if (page == 13)
+            {
+                Random randGen2 = new Random();
+                int randValue2 = randGen2.Next(1, 101);
+                if (randValue2 < 90)
+                {
+                    page = 15;
+                }
+                else
+                {
+                    page = 99;
+                }
+
+            }
+            
+            else if (page == 14) { page = 99; }
+            else if (page == 15) { page = 18; }
+            else if (page == 16) { page = 15; }
+            else if (page == 17) { page = 97; }
+            else if (page == 18) { page = 98; }
 
             else if (page == 99) { page = 96; }
             else if (page == 98) { page = 96; }
@@ -110,7 +145,8 @@ namespace LostAdventure
             ///to the page you need to go to if you selected option 2
 
             if (page == 3) {page = 5; }
-            else if (page == 5) { page = 7; }
+            else if (page == 4) { page = 10; }
+            else if (page == 5) { page = 10; }
 
 
             /// Display text and game options to screen based on the 
@@ -127,7 +163,7 @@ namespace LostAdventure
                     // Display initial message and options
                     outputLabel.Text = "It's time you become a trainer. Your mom kicks you out";
                     output2Label.Text = "Where will you go?";
-                    option1Label.Text = "Straight into the grass. No pokeballs. (55 % success)";
+                    option1Label.Text = "Straight into the grass. No pokeballs. (45 % success)";
                     option2Label.Text = "Oak's house";
                     option3Label.Visible = false;
                     option3Button.Visible = false;
@@ -139,9 +175,9 @@ namespace LostAdventure
                 case 2:
                     // Display page message and options
                     outputLabel.Text = "You get pecked to death by a pidgey. Idiot";
-
-                    option1Label.Text = "Yes";
-                    option2Label.Text = ".";
+                    output2Label.Text = "";
+                    option1Label.Text = "yes";
+                    option2Label.Text = "...";
                     SoundPlayer brookPlayer = new SoundPlayer(Properties.Resources.brook);
                     brookPlayer.Play();
 
@@ -164,17 +200,17 @@ namespace LostAdventure
                     output2Label.Text = "What will you do in Viridian City?";
                     option1Label.Text = "Talk to the local elderly men";
                     option2Label.Text = "Challenge the gym (diifcult)";
-                    option3Label.Text = "Move on to the next city (which is a better fit for your level)";
+                    option3Label.Text = "Move on to the next city (better fit for your level)";
                     option3Label.Visible = true;
                     option3Button.Visible = true;
                     break;
                 case 5:
                     // Display page message and options
-                    outputLabel.Text = $"Your pokemon gets you through the route safely. What will you do in Viridian City?";
+                    outputLabel.Text = "You make it to Viridian safely. What's your next move?";
                     output2Label.Text = "What will you do in Viridian City?";
                     option1Label.Text = "Talk to the local elderly men";
-                    option2Label.Text = "Challenge the gym (diifcult)";
-                    option3Label.Text = "Move on to the next city (which is a better fit for your level)";
+                    option2Label.Text = "Challenge the gym (difficult)";
+                    option3Label.Text = "Move on to the next city (better fit for your level)";
                     option3Label.Visible = true;
                     option3Button.Visible = true;
                     break;
@@ -182,37 +218,39 @@ namespace LostAdventure
                     // Display page message and options
                     outputLabel.Text = $"He tells you suspiciously that he knows a place in Celadon where you can get rich (quick!), and that he's going there soon and can take you.";
                     output2Label.Text = "Do you go to Celadon City with him? (OLD MAN)";
-                    option1Label.Text = "Yes";
-                    option2Label.Text = "No";
+                    option1Label.Text = "Yea lol";
+                    option2Label.Text = "No way? Dude";
                     option3Label.Visible = false;
                     option3Button.Visible = false;
                     break;
                 case 7:
                     // Display page message and options
-                    outputLabel.Text = $"It's much higher than your current skill level. The gym leader is also a gang boss. Do you still challenge Giovanni?";
+                    outputLabel.Text = $"It's much higher than your current skill level. The gym leader is also an infamous gang boss. Do you still challenge Giovanni?";
                     output2Label.Text = $"Do you still want to challenge the gym?";
-                    option1Label.Text = "Yes";
-                    option2Label.Text = "No";
+                    option1Label.Text = "I can take him";
+                    option2Label.Text = "What? No";
                     break;
                 case 8:
                     outputLabel.Text = $"You get rich as promised and completely forget about your old life. You can't remember why you ever wanted to be a pokemon trainer at all. Play again?";
                     output2Label.Text = $"OLD MAN ENDING";
-                    option1Label.Text = ".";
-                    option2Label.Text = ".";
+                    option1Label.Text = "";
+                    option2Label.Text = "";
                     break;
                 case 9:
-                    outputLabel.Text = $"Your sad level 9 team gets swept. Try again from the beginning.";
-                    output2Label.Text = $"Play again??";
-                    option1Label.Text = "Yes";
-                    option2Label.Text = "No";
+                    outputLabel.Text = $"Your sad level 9 team gets swept. Don't know what you expected.";
+                    output2Label.Text = $"NOOB ENDING";
+                    option1Label.Text = ":(";
+                    option2Label.Text = ":{";
                     break;
 
 
                 case 10:
                     outputLabel.Text = "You find yourself in Viridian forest.";
                     output2Label.Text = "What will you do in Viridian Forest?";
-                    option1Label.Text = "Beat up the youngsters while procedurally working through the forest";
+                    option1Label.Text = "Beat up all the youngsters, coldly and methodically";
                     option2Label.Text = "Avoid battles, opting to walk through the grass";
+                    option3Label.Visible = false;
+                    option3Button.Visible = false;
                     break;
                 case 11:
                     outputLabel.Text = "You beat all the trainers in the area. Bug Catchers are now an endangered species.";
@@ -227,7 +265,7 @@ namespace LostAdventure
                     option2Label.Text = "Yes";
                     break;
                 case 13:
-                    outputLabel.Text = "Uh oh. Its a level 100 Nidoking";
+                    outputLabel.Text = "Uh oh. It's a level 100 Nidoking";
                     output2Label.Text = "Do you fight, and attempt to catch it, or run?";
                     option1Label.Text = "Fight";
                     option2Label.Text = "Run";
@@ -253,14 +291,14 @@ namespace LostAdventure
                 case 17:
                     outputLabel.Text = "She lied?? theres no opportunity at team Rocket. You spend the rest if your life as a measly rocket grunt, never promoted";
                     output2Label.Text = "ROCKET ENDING";
-                    option1Label.Text = " ";
-                    option2Label.Text = " ";
+                    option1Label.Text = "";
+                    option2Label.Text = "";
                     break;
                 case 18:
                     outputLabel.Text = "Your good sense and reason have brought you this far. You challenge Brock and win!";
                     output2Label.Text = "GYM LEADER ENDING";
-                    option1Label.Text = " ";
-                    option2Label.Text = " ";
+                    option1Label.Text = "";
+                    option2Label.Text = "";
                     break;
 
                 case 99:
